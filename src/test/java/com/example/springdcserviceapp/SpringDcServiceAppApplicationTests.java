ea58bba953e368dfee0499e09ac9a81195eba4e5
Silverkadiusz55455 <arkadiusz55455@gmail.com>
2022-01-10 19:27:54 +0100

    #1

---
+++ src/test/java/com/example/springdcserviceapp/SpringDcServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdcserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDcServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
